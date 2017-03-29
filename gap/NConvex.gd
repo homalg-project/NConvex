0c2f00a24a03eea606d0740afc3e0bd45557f80b
kamalsaleh <ksaleh87@yahoo.com>
2017-03-29 09:03:42 +0200

    delete unneccesary function

--- gap/NConvex.gd
+++
@@ -1,9 +0,0 @@
-#
-# NConvex: new version of the Package Convex
-#
-# Declarations
-#
-
-#! @Description
-#!   Insert documentation for you function here
-DeclareGlobalFunction( "NConvex_Example" );
