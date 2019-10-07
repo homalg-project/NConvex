cb71c8eaa5255a0d6b18ac3f5bf262c44cee5bba
Kamal Saleh <ksaleh87@yahoo.com>
2019-10-07 17:01:54 +0200

    update the documentation of polytope operations

--- gap/NPolytope.gd
+++ gap/NPolytope.gd
@@ -23,19 +23,20 @@ DeclareCategory( "IsPolytope",
 #! @Chapter Polytopes
 #! @Section Creating polytopes
 
-#! @Arguments arg 
-#! @Returns a **Polytope** Object
+#! @Arguments L
+#! @Returns a Polytope Object
 #! @Description  
-#! The function takes a list of lists $[L_1, L_2, ...]$ where each $L_j$ represents 
+#! The operation takes a list $L$ of lists $[L_1, L_2, ...]$ where each $L_j$ represents 
 #! an inequality and returns the polytope defined by them (if they define a polytope). 
 #! For example the $j$'th entry $L_j = [c_j,a_{j1},a_{j2},...,a_{jn}]$ corresponds to the inequality
 #! $c_j+\sum_{i=1}^n a_{ji}x_i \geq 0$.
 DeclareOperation( "PolytopeByInequalities",
                   [ IsList ] );
-#! @Arguments arg 
-#! @Returns a **Polytope** Object
+
+#! @Arguments L
+#! @Returns a Polytope Object
 #! @Description  
-#! The function takes the list of the vertices and returns the polytope defined by them.
+#! The operation takes the list of the vertices and returns the polytope defined by them.
 DeclareOperation( "Polytope",
                   [ IsList ] );
 
@@ -48,39 +49,39 @@ DeclareOperation( "Polytope",
 
 #! @Section Attributes
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a CddPolyhedron
 #! @Description  
-#! Converts the polytope to a CddPolyhedron. The functions of CddInterface can then be applied
+#! Converts the polytope to a CddPolyhedron. The operations of CddInterface can then be applied
 #! on this polyhedron.
 DeclareAttribute( "ExternalCddPolytope",
                     IsPolytope );
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a List
 #! @Description  
-#! The function returns the list of integer points inside the polytope.                    
+#! The operation returns the list of integer points inside the polytope.                    
 DeclareAttribute( "LatticePoints",
                     IsPolytope );
   
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a List
 #! @Description  
-#! The function returns the interior lattice points inside the polytope.                    
+#! The operation returns the interior lattice points inside the polytope.                    
 DeclareAttribute( "RelativeInteriorLatticePoints",
                     IsPolytope );
  
 DeclareAttribute( "LatticePointsGenerators",
                     IsPolytope );
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a list of lists
 #! @Description  
-#! The function returns the vertices of the polytope  
+#! The operation returns the vertices of the polytope  
 DeclareAttribute( "VerticesOfPolytope",
                     IsPolytope );
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a list of lists
 #! @Description  
 #! The same output as <C>VerticesOfPolytope</C>.
@@ -90,10 +91,10 @@ DeclareOperation( "Vertices",
 DeclareOperation( "HasVertices",
                   [ IsPolytope ] );
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a list of lists
 #! @Description  
-#! The function returns the defining inequalities of the polytope.
+#! The operation returns the defining inequalities of the polytope.
 #! I.e., a list of lists $[L_1, L_2, ...]$ where each 
 #! $L_j=[c_j,a_{j1},a_{j2},...,a_{jn}]$ represents the inequality 
 #! $c_j+\sum_{i=1}^n a_{ji}x_i \geq 0$. If $L$ and $-L$ occur in the 
@@ -101,47 +102,47 @@ DeclareOperation( "HasVertices",
 DeclareAttribute( "DefiningInequalities",
                     IsPolytope );                    
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a list of lists
 #! @Description
-#! The function returns the defining-equalities of the polytope.
+#! The operation returns the defining-equalities of the polytope.
 DeclareAttribute( "EqualitiesOfPolytope",
                     IsPolytope );
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a list of lists
 #! @Description  
-#! The function returns the list of the inequalities of the facets.
+#! The operation returns the list of the inequalities of the facets.
 #! Each defining inequality that is not defining-equality of the 
 #! polytope is a facet inequality.
 DeclareAttribute( "FacetInequalities",
                     IsPolytope );
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a list of lists
 #! @Description  
-#! The function returns list of lists $L$. The entries of each $L_j$
+#! The operation returns list of lists $L$. The entries of each $L_j$
 #! in $L$ consists of $0$'s or $1$'s. For instance, if $L_j=[1,0,0,1,0,1]$, then
 #! The polytope has $6$ vertices and the vertices of the $j$'th facet are $\{V_1,V_4,V_6\}$.
 DeclareAttribute( "VerticesInFacets",
                     IsPolytope );
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a fan
 #! @Description  
-#! The function returns the normal fan of the given polytope.
+#! The operation returns the normal fan of the given polytope.
 DeclareAttribute( "NormalFan",
                     IsPolytope );
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a fan
 #! @Description
-#! The function returns the face fan of the given polytope. Remember that the face fan of a polytope is isomorphic to the normal fan of its
+#! The operation returns the face fan of the given polytope. Remember that the face fan of a polytope is isomorphic to the normal fan of its
 #! polar polytope.
 DeclareAttribute( "FaceFan",
                     IsPolytope );
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a cone
 #! @Description  
 #! If the ambient space of the polytope is $\mathrm{R}^n$, then the output is a cone in 
@@ -154,17 +155,17 @@ DeclareAttribute( "AffineCone",
 DeclareAttribute( "BabyPolytope",
                     IsPolytope );
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a Polytope
 #! @Description  
-#! The function returns the polar polytope of the given polytope.                   
+#! The operation returns the polar polytope of the given polytope.                   
 DeclareAttribute( "PolarPolytope",
                     IsPolytope );
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a Polytope
 #! @Description  
-#! The function returns the dual polytope of the given polytope.
+#! The operation returns the dual polytope of the given polytope.
 DeclareAttribute( "DualPolytope",
                     IsPolytope );
 
@@ -180,82 +181,82 @@ DeclareAttribute( "FVector", IsPolytope );
 
 #! @Section Properties
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a true or false
 #! @Description  
-#! returns if the polytope empty or not
+#! Returns whether the polytope empty or not
 DeclareProperty( "IsEmpty",
                  IsPolytope );
 
 DeclareProperty( "IsNotEmpty",
                  IsPolytope );
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a true or false
 #! @Description  
-#! returns if the polytope is lattice polytope or not.
+#! Returns whether the polytope is lattice polytope or not.
 DeclareProperty( "IsLatticePolytope",
                  IsPolytope );
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a true or false
 #! @Description  
-#! returns if the polytope is very ample or not.
+#! Returns whether the polytope is very ample or not.
 DeclareProperty( "IsVeryAmple",
                  IsPolytope );
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a true or false
 #! @Description  
-#! returns if the polytope is normal or not.
+#! Returns whether the polytope is normal or not.
 DeclareProperty( "IsNormalPolytope",
                  IsPolytope );
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a true or false
 #! @Description  
-#! returns if the polytope is simplicial or not.
+#! Returns whether the polytope is simplicial or not.
 DeclareProperty( "IsSimplicial",
                  IsPolytope );
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a true or false
 #! @Description  
-#! returns if the polytope is simplex polytope or not.
+#! Returns whether the polytope is simplex polytope or not.
 DeclareProperty( "IsSimplexPolytope",
                  IsPolytope );
                  
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a true or false
 #! @Description  
-#! returns if the polytope is simple or not.
+#! Returns whether the polytope is simple or not.
 DeclareProperty( "IsSimplePolytope",
                  IsPolytope );
                  
 DeclareProperty( "IsBounded",
                  IsPolytope );
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a true or false
 #! @Description  
-#! returns if the polytope is reflexive or not, i.e., if its dual polytope is lattice
+#! Returns whether the polytope is reflexive or not, i.e., if its dual polytope is lattice
 #! polytope.
 DeclareProperty( "IsReflexive", IsPolytope );
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a true or false
 #! @Description  
 #! returns whether the polytope is Fano or not. Fano polytope is a full dimensional lattice polytope whose vertices are 
 #! primitive elements in the containing lattice, i.e., each vertex is not a positive integer multiple of any other lattice element.
 DeclareProperty( "IsFanoPolytope", IsPolytope );
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a true or false
 #! @Description  
 #! returns whether the polytope is canonical Fano or not. A canonical Fano polytope is a full dimensional lattice polytope whose relative
 #! interior contains only one lattice point, namely the origin.
 DeclareProperty( "IsCanonicalFanoPolytope", IsPolytope );
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a true or false
 #! @Description  
 #! returns whether the polytope is terminal Fano or not. A terminal Fano polytope is a full dimensional lattice polytope whose
@@ -264,10 +265,10 @@ DeclareProperty( "IsTerminalFanoPolytope", IsPolytope );
 
 
 
-#! @Arguments polytope 
+#! @Arguments P 
 #! @Returns a true or false
 #! @Description  
-#! returns if the polytope is smooth fano polytope or not, i.e, if the vertices in each facet form a basis for the containing lattice or not.
+#! Returns whether the polytope is smooth fano polytope or not, i.e, if the vertices in each facet form a basis for the containing lattice or not.
 #! polytope.
 DeclareProperty( "IsSmoothFanoPolytope", IsPolytope );
 
@@ -279,7 +280,7 @@ DeclareProperty( "IsSmoothFanoPolytope", IsPolytope );
 
 #! @Section Operations on polytopes
 
-# @Arguments polytope1, polytope2
+# @Arguments P1, P2
 # @Returns a polytope
 # @Description
 # The output is cartesian product of the input polytopes. The cartesian product of two polytopes 
@@ -287,14 +288,14 @@ DeclareProperty( "IsSmoothFanoPolytope", IsPolytope );
 DeclareOperation( "\*",
                   [ IsPolytope, IsPolytope ] );
 
-#! @Arguments polytope1, polytope2
+#! @Arguments P1, P2
 #! @Returns a polytope
 #! @Description
 #! The output is Minkowski sum of the input polytopes.
 DeclareOperation( "\+",
                   [ IsPolytope, IsPolytope ] );
 
-#! @Arguments n, polytope
+#! @Arguments n, P
 #! @Returns a polytope
 #! @Description
 #! The output is Minkowski sum of the input polytope with itself $n$ times.
@@ -304,14 +305,14 @@ DeclareOperation( "\*",
 DeclareOperation( "\*",
                   [ IsPolytope, IsInt ] );
 
-# @Arguments polytope1, polytope2
+# @Arguments P1, P2
 # @Returns a polytope
 # @Description
 # The output The free sum  of the given polytopes. The free sum of two polytopes $P$ and $Q$ is
 # defined by $\{(M,0),M\in P\} \cub \{ (0,N),N\in Q\}$.
 DeclareOperation( "FreeSumOfPolytopes", [ IsPolytope, IsPolytope ] );
 
-#! @Arguments polytope1, polytope2
+#! @Arguments P1, P2
 #! @Returns a polytope
 #! @Description
 #! The output is the intersection of the input polytopes. 
@@ -324,14 +325,14 @@ DeclareOperation( "Points",
 DeclareOperation( "FourierProjection", 
                   [ IsPolytope, IsInt ] );
 
-#! @Arguments polytope
+#! @Arguments P
 #! @Returns a list
 #! @Description
 #! Returns a random interior point in the polytope.
 DeclareOperation( "RandomInteriorPoint", 
                   [ IsPolytope ] );
 
-#! @Arguments M, polytope
+#! @Arguments M, P
 #! @Returns true or false
 #! @Description
 #! Checks if the given point is interior point of the polytope.
